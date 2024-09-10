6f623d4ecc169219653e970329406dbbce469cd7
Ihor Vilkov <ihor.vilkov@nerdysoft.com>
2024-09-10 11:47:24 +0300

    added tests for transaction service

--- wallet-service/src/main/java/com/nerdysoft/walletservice/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/nerdysoft/walletservice/WalletServiceApplication.java
@@ -2,10 +2,10 @@ package com.nerdysoft.walletservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class WalletServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WalletServiceApplication.class, args);
