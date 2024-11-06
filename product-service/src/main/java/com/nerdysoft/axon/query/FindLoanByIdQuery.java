11ac55d39a767f0b16b075874a85b5f48b3af8df
Yaroslav Radevych-Vynnytskyi <124635285+YaroslavRadevychVynnytskyi@users.noreply.github.com>
2024-11-06 13:47:10 +0200

    added @NoArgsConstructor annotation to product-service queries (#23)

--- product-service/src/main/java/com/nerdysoft/axon/query/FindLoanByIdQuery.java
+++ product-service/src/main/java/com/nerdysoft/axon/query/FindLoanByIdQuery.java
@@ -2,10 +2,12 @@ package com.nerdysoft.axon.query;
 
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@NoAgrsConstructor
 public class FindLoanByIdQuery {
-    private final UUID loanId;
+    private UUID loanId;
 }
--- wallet-shared/src/main/java/com/nerdysoft/axon/event/loan/RepayLoanEvent.java
+++ wallet-shared/src/main/java/com/nerdysoft/axon/event/loan/RepayLoanEvent.java
@@ -2,10 +2,12 @@ package com.nerdysoft.axon.event.loan;
 
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RepayLoanEvent {
     private UUID accountId;
 }
